953f04f5797259ad858b34eb6b471b83d52e47f9
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-30 09:48:39 +0100

    removed the default state code defined here and moved it to app.js

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,15 +1,6 @@
 import React, {useState} from 'react'
 
 const TodoItem = () => {
-  // set default state
-  // initial state for all items
-[items, setItems] = useState([]);
-// initial state of id before an item is created
-[id, setId] = useState(0);
-// initial state of the item field to be created
-[item, setItem] = useState('');
-//initial state before item is edited
-[editItem, setEditItem] = useState(false)
   return (
    <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
      <h6>Title</h6>
