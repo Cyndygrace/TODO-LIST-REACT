44ff9de54312647f6ac4ceffbf7fb846106ebef3
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-30 11:09:42 +0100

    add an onChange event listener and pass the handle change method as a value

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -12,7 +12,7 @@ const TodoInput = (props) => {
       </div>
     </div>
     {/* by setting value={item}, we are initializing the value of value to the value of state. the handle CHange method is now used to set new value to the value of input value so that the new input value is displayed on the browser when something is typed */}
-    <input type="text" className="form-control text-capitalize" placeholder="add a todo item" value={item} 
+    <input type="text" className="form-control text-capitalize" placeholder="add a todo item" value={item} onChange={handleChange}
     />
     </div>
     <button type="submit" className="btn btn-block btn-primary mt-3">add item</button>
