d070b8058638c9fe81806f94d1379318b5b4c407
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-30 03:18:24 +0100

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import TodoList from '../src/Components/TodoList'
 import TodoInput from '../src/Components/TodoInput'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
