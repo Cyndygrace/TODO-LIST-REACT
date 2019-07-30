c037c35d4fcc28aca32c66ea473dc527abf6586b
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-30 09:15:01 +0100

    imported the components to render on the browser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
-import TodoList from '../src/Components/TodoList'
-import TodoInput from '../src/Components/TodoInput'
+import TodoList from '../src/Components/TodoList';
+import TodoInput from '../src/Components/TodoInput';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import uuid from 'uuid'
+import uuid from 'uuid';
 function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-10 mx-auto col-md-8 mt-4"></div>
-        <h3 className="text-capitalize text-center"> todo input </h3>
+        <div className="col-10 mx-auto col-md-8 mt-4">
+          <h3 className="text-capitalize text-center"> todo input </h3>
+          <TodoInput />
+          <TodoList />
+        </div>
       </div>
-      <TodoInput />
-      <TodoList />
     </div>
   );
 }
