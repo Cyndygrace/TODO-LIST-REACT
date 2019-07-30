209729948db767feddb046bed78a518416da4261
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-30 09:11:50 +0100

    added jsx code snippets to display TodoInput to the browser

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const TodoInput = () => {
   return (
-    <div>
-      hello form todo input
+    <div className="card card-body my-3">
+      <form>
+      <div className="input-group">
+      <div className="input-group-prepend">
+        <div className="input-group-text bg-primary text-white" >
+        <i className="fas fa-book" />
+      </div>
     </div>
-  )
-}
+    <input type="text" className="form-control text-capitalize" placeholder="add a todo item"
+    />
+    </div>
+    <button type="submit" className="btn btn-block btn-primary mt-3">add item</button>
+     </form>
+    </div>
+  );
+};
 
-
-export default TodoInput
+export default TodoInput;
